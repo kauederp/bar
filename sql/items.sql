6be9711b5f1b8e7ad9661472744431961285db6b
kauederp <36017642+kauederp@users.noreply.github.com>
2022-06-07 12:07:11 -0300

    Add files via upload

---
+++ sql/items.sql
@@ -0,0 +1,115 @@
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'bombom', '0.50', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'bombom', '1', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'biscoito hot peito de peru', '1', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'bala canudinho regaliz citrico', '1', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'club social', '1', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'italakinho', '1', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'água com gás', '1', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'baton', '1', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'bisc bauduco', '1', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'freegells', '1', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'disqueti', '1', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'pastilha minty', '1', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'mandolate', '1', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'torrone', '1', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'refeição', '1', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'azedinho', '1', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'amendoim', '1.50', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'sucos', '1.50', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'bibs dark', '1.50', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'coca cola', '1.50', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'sprite', '1.50', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'pitulito', '1.50', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'barra de cereal', '1.50', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'choc lacta(diam negro, oreo,leite)', '1.50', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'chokito', '2.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'nescau', '2.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'stikadinho', '2.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'trident', '2.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'suco', '2.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'trento', '2.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'cápsula', '2.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'petisk zezé', '2.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, '5 star', '2.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'ice tea', '2.5', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'bibs', '2.5', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'coca café', '2.5', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'pepsi zero/fruki lata', '2.5', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'bolacha recheada', '2.5', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'snickers', '2.5', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'bis extra', '2.5', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'twix', '3.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'fanta 350ml lata', '2.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'kit kat', '2.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'h2o', '2.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'tonica', '2.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'nutella', '2.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'biscoito oreo recheado', '3.5', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'amstel', '4.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'brahma', '4.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'budweiser', '4.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'heineken', '4.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'refri 600', '4.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'cookies', '4.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'sardinha', '4.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'gatorade', '4.5', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'baly', '6.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'monster', '8.0', '0');
+
+INSERT INTO `items` (`id`, `nome`, `preco`, `quantidade`) VALUES (NULL, 'red bull', '8.0', '0');
